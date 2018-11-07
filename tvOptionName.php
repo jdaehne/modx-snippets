a1a79b969e734713420d7048e7c936ae10f9a2fb
Jan Dähne <jan.daehne@quadro-system.de>
2018-11-07 18:36:19 +0100

    Add tvOptionName

---
+++ tvOptionName.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * tvOptionName
+ *
+ * returns the Name of a tv option (type: DropDown List or multi DropDown List)
+ *
+ * Usage examples:
+ * [[*tvName:tvOptionName=`65`]]
+ *
+ * @author Jan Dähne <jan.daehne@quadro-system.de>
+ */
+
+$tv = $options; // ID of TV
+
+$tv = $modx->getObject('modTemplateVar', $tv);
+$optionValues = $tv->get('elements');   
+$options = explode("||", $optionValues);
+
+foreach ($options as $option) {
+    $valueKey = explode("==", $option);
+    
+    if ($valueKey[1] == $input) {
+        return $valueKey[0];
+    }
+}
+
+return $input;
