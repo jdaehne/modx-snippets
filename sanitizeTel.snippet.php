3e2f54486ef285ea6e59d378c275673784472e93
Jan Dähne <jan.daehne@quadro-system.de>
2019-06-03 14:21:57 +0200

    Add sanitizeTel snippet

---
+++ sanitizeTel.snippet.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * sanitizeTel
+ *
+ * Sanitize phone numbers, to use in a <a href="tel:..." link.: +49 (0)12 345-67 will be converted to 00491234567.
+ *
+ * Usage examples:
+ * [[*tvName:sanitizeTel]]
+ *
+ * @author Jan Dähne <jan.daehne@quadro-system.de>
+ */
+
+$stripnumber = '';
+$output = '';
+
+if(isset($input) && $input !='') {
+
+    $stripbracketzero = preg_replace('~\(\d{1,2}\)~', '', $input);
+
+    $stripplus = preg_replace('~^\D++~', '00', $stripbracketzero);
+
+    $stripnumber = preg_replace('/\D/', '', $stripplus);
+
+    $output .= $stripnumber;
+}
+
+return $output;
